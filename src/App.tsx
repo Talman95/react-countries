4d7ed1e0ec356e11f86003ccf0fba8641cc40f3f
Talman95 <taltro528@gmail.com>
2023-05-22 11:59:20 +0300

    added gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
 
-import { AppRoutes } from './routes/AppRoutes';
-import { Header } from './shared/Header/Header';
-import { ToastPortal } from './shared/ToastPortal/ToastPortal';
+import { AppRoutes } from 'routes/AppRoutes';
+import { Header } from 'shared/Header/Header';
+import { ToastPortal } from 'shared/ToastPortal/ToastPortal';
 
 export const App: FC = () => {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-import { App } from './App';
-import { store } from './store/store';
+import { App } from 'App';
+import { store } from 'store/store';
 import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
